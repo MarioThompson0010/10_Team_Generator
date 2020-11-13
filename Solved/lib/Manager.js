e596202bbc23f0b286f77f2ef971784d3d5318fd
Mario Thompson <MarioThompson0010@gmail.com>
2020-11-13 06:56:35 -0600

    Correction of last commit: Cut from Manager file to Engineer file to create Engineer

--- Solved/lib/Manager.js
+++ Solved/lib/Manager.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber = 0){
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+
+    }
+
+    getRole(){
+        return this;
+    }
+}
+
+// const manager = new Manager("hi", 3, "m@gpd.com", 3);
+// const temp = manager.getRole();
+// const deb = 0;
