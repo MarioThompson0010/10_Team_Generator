015b70d262705458a0c371fe2e364ab25a9e6740
Mario Thompson <MarioThompson0010@gmail.com>
2020-11-13 16:51:14 -0600

    Added documentation and changed one word of verbiage.

--- Solved/app.js
+++ Solved/app.js
@@ -5,12 +5,12 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output"); // get full path to directory output
+const outputPath = path.join(OUTPUT_DIR, "team.html"); // append team.html to directory output folder
 
 const render = require("./lib/htmlRenderer");
 
-let responseFromUser = ""; // get value from while loop to know when to quit
+//let responseFromUser = ""; // get value from while loop to know when to quit
 
 const employeeQuestions = {
     employeeName: {
@@ -96,7 +96,7 @@ function getInformationEmployee() {
                             .then((responseEmployeeType) => {
 
                                 const data = responseEmployeeType;
-                                responseFromUser = responseEmployeeType.EmployeePrompt;
+                                //responseFromUser = responseEmployeeType.EmployeePrompt;
                                 if (responseEmployeeType.EmployeePrompt !== 'Exit') {
 
                                     switch (responseEmployeeType.EmployeePrompt) {
@@ -139,7 +139,7 @@ function getInformationEmployee() {
                                                     employeeQuestions.employeeEmail,
                                                     {
                                                         type: 'input',
-                                                        message: 'What is your Github url?',
+                                                        message: 'What is the Github url?',
                                                         name: 'GithubPrompt'
                                                     }
                                                 ]
@@ -185,33 +185,12 @@ function getInformationEmployee() {
     askManager();
 }
 
+// write to the output file.
 function writeToFile(fileName, rendered) {
     fs.writeFile(fileName, rendered, (err) =>
         err ? console.log(err) : console.log('Success!')
     );
 }
 
-
+// get the show started with this
 getInformationEmployee();
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
